0d35d988194e7a931f173dd00060aa6f827a57dc
JS <49812749+JS00001@users.noreply.github.com>
2025-01-21 10:58:19 -0500

    tracking

--- src/middleware/tracking.ts
+++ src/middleware/tracking.ts
@@ -2,8 +2,10 @@ import { Request, Response, NextFunction } from 'express';
 
 import { ErrorHandler } from '@/lib/error';
 
+const REFRESH_RATE_MS = 60000; // How often to update the last online time
+
 /**
- * For every request, if the chapter has not made a request in the last 1 minute,
+ * For every request, if the user has not made a request in the last 1 minute,
  * update it to the current time.
  */
 const updateLastOnline = async (
@@ -15,26 +17,27 @@ const updateLastOnline = async (
     const user = req.user!;
     const clientVersion = req.headers['x-client-version'] as string | undefined;
 
-    const REFRESH_RATE_MS = 60000; // How often to update the last online time
-
     const currentTimeMillis = new Date().getTime();
     const lastOnlineTimeMillis = user.lastOnlineAt.getTime();
 
-    const shouldUpdateClientVersion =
-      clientVersion && user.clientVersion !== clientVersion;
-
+    // Update the 'last online' time if needed
     const shouldUpdateLastOnline =
       lastOnlineTimeMillis + REFRESH_RATE_MS < currentTimeMillis;
 
+    if (shouldUpdateLastOnline) {
+      user.lastOnlineAt = new Date();
+    }
+
+    // Update the client version if needed
+    const shouldUpdateClientVersion =
+      clientVersion && user.clientVersion !== clientVersion;
+
+    if (shouldUpdateClientVersion) {
+      user.clientVersion = clientVersion;
+    }
+
+    // Save the user if needed
     const shouldSaveUser = shouldUpdateClientVersion || shouldUpdateLastOnline;
-
-    // Update the last online time if it has been more than 1 minute
-    user.lastOnlineAt = shouldUpdateLastOnline ? new Date() : user.lastOnlineAt;
-
-    // Update the client version if it has changed
-    user.clientVersion = shouldUpdateClientVersion
-      ? clientVersion
-      : user.clientVersion;
 
     if (shouldSaveUser) {
       await user.save();
