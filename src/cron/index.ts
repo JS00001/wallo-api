62db944d6e3679dc1f5f82f372a6e326b75ba753
JS <49812749+JS00001@users.noreply.github.com>
2025-01-23 22:19:16 -0500

    cleanup cron job details

--- src/cron/index.ts
+++ src/cron/index.ts
@@ -7,22 +7,26 @@ import regenerateLives from './jobs/regenerate-lives';
 import clearWeekData from './jobs/clear-week-data';
 
 const startJobs = () => {
-  // Every 2 hours - Regenerate lives to full
+  // Every 2 hours
+  // Regenerate lives to full
   cron.schedule('0 */2 * * *', regenerateLives);
 
-  // Every day at 6:00pm - Send daily reminder to do streaks
+  // Every day at 6:00pm
+  // Send daily reminder to do streaks
   cron.schedule('0 18 * * *', dailyReminder);
 
-  // Every day at 12:00am - Reset daily rewards and update expired streaks
+  // Every hour of Sundays (in the job, limits to midnight for each timezone)
+  // Reset week's streak data for each timezone
+  cron.schedule('0 * * * 0', clearWeekData);
+
+  // Every hour (in the job, limits to midnight for each timezone)
+  // Reset daily rewards and update expired streaks
   cron.schedule('0 * * * *', async () => {
     // Update streaks first because we rely on data from daily challenges that
     // get cleared in resetChallenges
     await updateStreaks();
     await resetChallenges();
   });
-
-  // Every hour of Sundays - Reset week's streak data
-  cron.schedule('0 * * * 0', clearWeekData);
 };
 
 export default { startJobs };
